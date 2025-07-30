22170861599facd3207b9c6592ad889eb5c1db4e
mayur7267 <mayurlate.7267@gmail.com>
2025-07-30 23:00:48 +0530

    Initial Commit

---
+++ ColorApp/ColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppApp.swift
+//  ColorApp
+//
+//  Created by Mayur on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorAppTests/ColorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppTests.swift
+//  ColorAppTests
+//
+//  Created by Mayur on 30/07/25.
+//
+
+import Testing
+@testable import ColorApp
+
+struct ColorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
